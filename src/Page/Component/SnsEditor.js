e95b84395f5aba59fd86f7949ecce84a59b48c1d
Chang Liu <cl3@student.unimelb.edu.au>
2020-05-10 21:38:11 +1000

    Mirgrating..

---
+++ src/Page/Component/SnsEditor.js
@@ -0,0 +1,84 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import "rc-color-picker/assets/index.css";
+import BackgroundControl from "./BackgroundControl.js";
+
+const styles = (theme) => ({});
+
+class SnsEditor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sharingurl: props.sharingurl || "",
+    };
+    
+    this.background = props.background || null;
+  }
+
+  getProps() {
+    return {
+      sharingurl: this.state.sharingurl,
+      background: this.background,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value, // update the changed value
+    });
+  }
+
+  handlePureChange = (event) => {
+    this[event.target.name] = event.target.value;
+  }
+
+  render() {
+    return (
+      <Dialog
+        open={this.props.open}
+        fullWidth={true}
+        maxWidth={"lg"}
+        onClose={this.props.onClose}
+      >
+        <MuiDialogContent>
+          <TextField
+            fullWidth
+            id="videourl"
+            variant="outlined"
+            name="videourl"
+            label="VideoURL"
+            value={this.state.sharingurl}
+            onChange={this.handleChange}
+          />
+          <BackgroundControl
+            {...this.props.background}
+            name="background"
+            onChange={this.handlePureChange}
+          />
+        </MuiDialogContent>
+        <MuiDialogActions>
+          <Button autoFocus onClick={this.props.onClose}>
+            Cancel
+          </Button>
+          <Button
+            autoFocus
+            onClick={() => {
+              this.props.saveComponent(this.getProps());
+            }}
+            color="primary"
+          >
+            Save
+          </Button>
+        </MuiDialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default withStyles(styles)(SnsEditor);
